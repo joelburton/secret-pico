0705489798c6ded9d54f785e7c86b3421f6ba87a
Joel Burton <joel@joelburton.com>
2021-03-05 09:08:17 -0800

    Tidy up deploy stuff

--- server/conf.py
+++ server/conf.py
@@ -1,33 +1,7 @@
 import os
 from glide.conf import *
 
-# -- Project information -----------------------------------------------------
-
 project = "Secret Pico"
-# noinspection PyShadowingBuiltins
 copyright = 'Joel Burton and Fluffy Labs'
 author = 'Joel Burton'
-
-# The short X.Y version
-version = os.environ.get("RITHM_COHORT")
-# The full version, including alpha/beta/rc tags
-release = f'March 2021'
-
-# -- General configuration ---------------------------------------------------
-
-# RST Prolog: this stuff is added to every RST file before it's processed
-_curric_name = f"""
-.. |curric-name|   replace:: {os.environ.get('CURRIC_NAME')}
-"""
-
-rst_prolog += _curric_name
-
-# -- Options for HTML output -------------------------------------------------
-
-# revealjs_theme = "revealjs-rithm"
-# html_theme = "handouts-rithm"
-
-# -- Extension configuration -------------------------------------------------
-
-# If false, the to-do and to-do-list directives produce nothing
-todo_include_todos = True
+version = release = f'March 2021'
